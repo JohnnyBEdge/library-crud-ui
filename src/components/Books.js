99c3f6bb812f306b451ef861bf87f0ed0d6ce90b
John Martin <jrmartin182@gmail.com>
2020-04-20 10:57:21 -0600

    added minimal UI to list and view a single object from each data set

---
+++ src/components/Books.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Books extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            books: []
+        }
+    }
+
+    getBooks(){
+        fetch('http://localhost:5000/api/books')
+        .then(response => response.json())
+        .then(data => this.setState({books:data}, () => console.log(this.state.books))) 
+    };
+
+    componentDidMount(){
+        this.getBooks();
+    };
+
+    render(){
+
+        const book = this.state.books.map(book =>
+            <ul>
+                <li>{book.title}</li>
+            </ul>
+        );
+
+        return(
+            <>
+                {book}
+            </>
+            
+        );
+    };
+};
+
+export default Books
